2aa1e7be125818d19c7cdfb5894d26c6ecfe00cc
ZadAndRot <kasirecivanna@gmail.com>
2022-07-04 23:30:01 +0300

    file added

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import Container from "container";
+
+export const App = () => {
+  return (
+    <div 
+      style={{
+        height: 'auto',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        backgroundColor:'lightgray',
+        position: "absolute",
+        width: "100vw"
+      }}
+    >
+    
+    <Container/>
+
+    
+    
+     
+    </div>
+  );
+};
